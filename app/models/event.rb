74a14920dc8aa77889bd378f5ff85d1a438ddf1e
Husnain Siddique <husnainsiddique581@gmail.com>
2023-12-08 08:11:11 +0500

    unique validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord 
   belongs_to :organizer, class_name: 'User', foreign_key: 'organizer_id'
-  has_many :event_users, class_name: 'EventUser',dependent: :destroy
+  has_many :event_users, class_name: 'EventUser', dependent: :destroy
   has_many :users, class_name: 'User', through: :event_users
 
   validates :name, presence: true, length: { maximum: 30 }
