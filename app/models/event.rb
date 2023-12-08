545fb1f9d9541fe59e01827878dfc1b400d3efdf
Shujaat <shujaat222@gmail.com>
2023-12-07 23:23:21 -0500

    Fixed not joined events query issue

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,14 +15,7 @@ class Event < ApplicationRecord
     where(organizer_id: user.id)
   end
 
-  scope :not_organized_by_user, ->(user) do
-    where.not(organizer_id: user.id)
-  end
-
   scope :not_joined_by_user, ->(user) do
-    left_outer_joins(:event_users)
-    .where.not('event_users.user_id': user.id)
-    .or(Event.where('event_users.id': nil))
-    .distinct
+    where.not(id: user.events.ids | Event.organized_by_user(user).ids)
   end
 end
